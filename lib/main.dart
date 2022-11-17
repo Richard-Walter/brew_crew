8b9e05a670f72c6c10e4963bc28470a502c53358
Richard Walter <rjw.work@gmail.com>
2022-11-17 11:12:50 +1100

    initial

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:brew_crew/firebase_options.dart';
+import 'package:brew_crew/screens/wrapper.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const Wrapper()
+    );
+  }
+}
+
+
+
+
